82383084b781eebb4f4b14a9e9b3b0aa52afd3eb
viet-quoctran <67690248+viet-quoctran@users.noreply.github.com>
2024-05-09 23:21:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from slugify import slugify
 import sys
 import io
 
-# Thay đổi mã hóa toàn cục cho stdout để hỗ trợ UTF-8
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8', errors='ignore')
 
 def main():
